2674bcef4acc30136f0df8036586f400a44b3bc4
bmartin97 <barsimartin@gmail.com>
2018-10-10 19:06:29 +0200

    Tracking
    Tracking active window_ console app

---
+++ Tracking active window.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        [DllImport("user32.dll")]
+        static extern IntPtr GetForegroundWindow();
+        [DllImport("user32.dll")]
+        static extern int GetWindowText(IntPtr hWnd, StringBuilder text, int count);
+
+        public static string GetActiveWindowTitle()
+        {
+            const int nChars = 256;
+            StringBuilder Buff = new StringBuilder(nChars);
+            IntPtr handle = GetForegroundWindow();
+
+            if (GetWindowText(handle, Buff, nChars) > 0)
+            {
+                return Buff.ToString();
+            }
+            return null;
+        }
+        static void Main(string[] args)
+        {
+            string activeWindow = GetActiveWindowTitle();
+            string newWindow;
+            uint time = 0;
+            while (true)
+            {
+                newWindow = GetActiveWindowTitle();
+                if(newWindow != activeWindow)
+                {
+                    if(time>1)
+                        Console.WriteLine("[ "+ time + "s ]  " + activeWindow);
+                    activeWindow = newWindow;
+                    time = 0;
+                } else
+                {
+                    time++;
+                }
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
