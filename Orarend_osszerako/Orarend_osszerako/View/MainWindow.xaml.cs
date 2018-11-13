22edcdbee965209e76482e3a69c7162693a3398b
Unknown <barsimartin@gmail.com>
2018-11-13 13:42:42 +0100

    Material Design

--- Orarend_osszerako/Orarend_osszerako/View/MainWindow.xaml.cs
+++ Orarend_osszerako/Orarend_osszerako/View/MainWindow.xaml.cs
@@ -24,16 +24,17 @@ namespace Orarend_osszerako
         public MainWindow()
         {
             InitializeComponent();
+            Main.Content = new LoginPage(); //Alapból a login page-et tölti be
         }
 
         private void SignIn_Click(object sender, RoutedEventArgs e)
         {
-            Main.Content = new LoginPage();
+            Main.Content = new LoginPage(); // átvált LoginPage-re a <frame>
         }
 
         private void SignUp_Click(object sender, RoutedEventArgs e)
         {
-            Main.Content = new RegisterPage();
+            Main.Content = new RegisterPage(); // átvált RegisterPage-re a <frame>
         }
     }
 }
