a403b902bcf8623e890224fe09799e96f2124d20
Unknown <barsimartin@gmail.com>
2018-11-13 17:19:05 +0100

    Active tab style

--- Orarend_osszerako/Orarend_osszerako/View/MainWindow.xaml.cs
+++ Orarend_osszerako/Orarend_osszerako/View/MainWindow.xaml.cs
@@ -22,20 +22,28 @@ namespace Orarend_osszerako
     
     public partial class MainWindow : Window
     {
+        Style Primary = Application.Current.FindResource("MaterialDesignRaisedButton") as Style;
+        Style Secondary = Application.Current.FindResource("MaterialDesignFlatButton") as Style;
         public MainWindow()
         {
             InitializeComponent();
             Main.Content = new LoginPage(); //Alapból a login page-et tölti be
+            signin.Style = Primary;
+            signup.Style = Secondary;
         }
 
         private void SignIn_Click(object sender, RoutedEventArgs e)
         {
             Main.Content = new LoginPage(); // átvált LoginPage-re a <frame>
+            signin.Style = Primary;
+            signup.Style = Secondary;
         }
 
         private void SignUp_Click(object sender, RoutedEventArgs e)
         {
             Main.Content = new RegisterPage(); // átvált RegisterPage-re a <frame>
+            signup.Style = Primary;
+            signin.Style = Secondary;
         }
     }
 }
