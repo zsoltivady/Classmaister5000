0f12fc3372a93282026a8dbd2eb9ee216ee7279d
Daniuni <43348697+Daniuni@users.noreply.github.com>
2018-11-16 16:45:00 +0100

    Regisztráció validáció VM

--- Orarend_osszerako/Orarend_osszerako/Model/UserModel.cs
+++ Orarend_osszerako/Orarend_osszerako/Model/UserModel.cs
@@ -29,18 +29,7 @@ namespace Orarend_osszerako.Model
             get { return userId; }
             set { userId = value; }
         }
-        #region RegexHelperMethod
-        //public string FirstLetterToUpper(string str)
-        //{
-        //    if (str == null)
-        //        return null;
-
-        //    if (str.Length > 1)
-        //        return char.ToUpper(str[0]) + str.Substring(1);
-
-        //    return str.ToUpper();
-        //}
-        #endregion
+        
         private string firstName;
         public string FirstName
         {
@@ -48,28 +37,7 @@ namespace Orarend_osszerako.Model
             set
             {
                 firstName = value;
-                #region FirstNameRegex
-                //var hasNumber = new Regex(@"[0-9]+");
-                //var hasSymbols = new Regex(@"[!@#$%^&*()_+=\[{\]};:<>|./?,-]");                
-
-                //if (string.IsNullOrWhiteSpace(value))
-                //{
-                //    throw new Exception("A keresztnevet meg kell adni!");
-                //}               
-
-                //else if (hasNumber.IsMatch(value))
-                //{
-                //    throw new Exception("A keresztnév nem tartalmazhat számokat!");  
-                //}
-                //else if (hasSymbols.IsMatch(value))
-                //{
-                //    throw new Exception("A keresztnév nem tartalmazhat szimbólumokat!");
-                //}
-                //else 
-                //{
-                //    firstName = FirstLetterToUpper(value);
-                //}
-                #endregion
+                
             }
         }
         private string lastName;
@@ -79,28 +47,7 @@ namespace Orarend_osszerako.Model
             set
             {
                 lastName = value;
-                #region LastNameRegex
-                //var hasNumber = new Regex(@"[0-9]+");
-                //var hasSymbols = new Regex(@"[!@#$%^&*()_+=\[{\]};:<>|./?,-]");
-
-                //if (string.IsNullOrWhiteSpace(value))
-                //{
-                //    throw new Exception("A vezetéknevet meg kell adni!");
-                //}
-
-                //else if (hasNumber.IsMatch(value))
-                //{
-                //    throw new Exception("A vezetéknév nem tartalmazhat számokat!");
-                //}
-                //else if (hasSymbols.IsMatch(value))
-                //{
-                //    throw new Exception("A vezetéknév nem tartalmazhat szimbólumokat!");
-                //}
-                //else
-                //{
-                //    lastName = FirstLetterToUpper(value);
-                //}
-                #endregion
+                
             }
         }
         private string userName;
@@ -116,49 +63,7 @@ namespace Orarend_osszerako.Model
             set
             {
                 password = value;
-                #region PasswordRegex
-                //if (string.IsNullOrWhiteSpace(value))
-                //{
-                //    throw new Exception("A jelszót meg kell adni!");
-                //}
-
-                //var hasNumber = new Regex(@"[0-9]+");
-                //var hasUpperChar = new Regex(@"[A-Z]+");
-                //var hasMiniMaxChars = new Regex(@".{5,10}");
-                //var hasLowerChar = new Regex(@"[a-z]+");
-                //var hasSymbols = new Regex(@"[!@#$%^&*()_+=\[{\]};:<>|./?,-]");
-
-                //if (!hasLowerChar.IsMatch(value))
-                //{
-                //    throw new Exception("A jelszónak tartalmaznia kell legalább 1 kisbetűt!");
-
-                //}
-                //else if (!hasUpperChar.IsMatch(value))
-                //{
-                //    throw new Exception("A jelszónak tartalmaznia kell legalább 1 nagybetűt");
-
-                //}
-                //else if (!hasMiniMaxChars.IsMatch(value))
-                //{
-                //    throw new Exception("A jelszó legalább 5, maxiumum 10 karakter hosszú lehet!");
-
-                //}
-                //else if (!hasNumber.IsMatch(value))
-                //{
-                //    throw new Exception("A jelszónak tartalmaznia kell legalább 1 számot!");
-
-                //}
-
-                //else if (!hasSymbols.IsMatch(value))
-                //{
-                //    throw new Exception("A jelszónak tartalmaznia kell legalább 1 speciális karaktert!");
-
-                //}
-                //else
-                //{
-                //password = value;
-                //}
-                #endregion
+                
             }
 
         }
--- Orarend_osszerako/Orarend_osszerako/ViewModel/RegistrationViewModel.cs
+++ Orarend_osszerako/Orarend_osszerako/ViewModel/RegistrationViewModel.cs
@@ -12,10 +12,11 @@ using System.ComponentModel;
 using System.Collections;
 using System.ComponentModel.DataAnnotations;
 using Orarend_osszerako.BusinessLogic.Exceptions;
+using Orarend_osszerako.Mapper;
 
 namespace Orarend_osszerako.ViewModel
 {
-    public class RegistrationViewModel : INotifyPropertyChanged
+    public class RegistrationViewModel : INotifyPropertyChanged , INotifyDataErrorInfo
     {
         //a viewekben található textboxok tartalma ezekbe a propertykbe kerül be a bindingon keresztül
         private string _FirstName;
@@ -26,6 +27,7 @@ namespace Orarend_osszerako.ViewModel
             set
             {
                 _FirstName = value;
+                ValidateProperty("FirstName", value);
             }
         }
         private string _LastName;
@@ -36,6 +38,7 @@ namespace Orarend_osszerako.ViewModel
             set
             {
                 _LastName = value;
+                ValidateProperty("LastName", value);
             }
         }
         private string _UserName;
@@ -46,6 +49,7 @@ namespace Orarend_osszerako.ViewModel
             set
             {
                 _UserName = value;
+                ValidateProperty("UserName", value);
             }
         }
         private string _userPassword;
@@ -56,6 +60,7 @@ namespace Orarend_osszerako.ViewModel
             set
             {
                 _userPassword = value;
+                ValidateProperty("UserPassword", value);
             }
         }
         private string _retryPassword;
@@ -66,19 +71,11 @@ namespace Orarend_osszerako.ViewModel
             set
             {
                 _retryPassword = value;
+                ValidateProperty("RetryPassword", value);
             }
         }
-        //mivel még nincs implementálva a validáció, és ezzel a INotifyDataErrorInfo sincs
-        //public bool HasErrors => throw new NotImplementedException();
         
-        //public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
-
-        //public IEnumerable GetErrors(string propertyName)
-        //{
-        //    throw new NotImplementedException();
-        //}
-
-        public event PropertyChangedEventHandler PropertyChanged; //remélem az INotifyProperyChanged interface mezőit és metódusait nem kell magyarázni
+        public event PropertyChangedEventHandler PropertyChanged; 
         protected void NotifyPropertyChanged(String info)
         {
             if (PropertyChanged != null)
@@ -86,16 +83,153 @@ namespace Orarend_osszerako.ViewModel
                 PropertyChanged(this, new PropertyChangedEventArgs(info));
             }
         }
-        private ICommand _doRegistration; //itt hozzunk létre egy üres ICommandot 
+        private ICommand _doRegistration;
         public ICommand DoRegistration
         {
             get
             {
                 if (_doRegistration == null)
                 {
-                        _doRegistration = new RelayCommand(p => true, p => Registration.RegisterUser(UserName, UserPassword, RetryPassword, FirstName, LastName)); //p=> true azt mondja, hogy mindig futtatható az ICommand, ha szükség lesz rá, ezt logikai feltétellel vizsgálhatjuk, PL: Amíg nincsenek kitöltve egyáltán a TextBoxok, így a propertyk nem töltődtek fel, addig ne futtathassuk az ICommandot (tehát a gombnyomást), így nem terheljük (talán?) feleslegesen a programot
+                        _doRegistration = new RelayCommand(p => true, p => RegisterUser(UserName, UserPassword, RetryPassword, FirstName, LastName));
                 }
                 return _doRegistration;
+            }
+        }
+        public bool HasUserName(string username)
+        {
+            using (var context = new Classmaister5000Entities())
+            {
+                var hasuser = context.Users.Any(u => u.UserName.ToLower() == username.ToLower());
+                return hasuser;
+            }
+        }
+        public void RegisterUser(string username, string password, string RetryPassowrd,
+            string FirstName, string LastName)
+        {
+            using (var context = new Classmaister5000Entities())
+            {
+                Validate();
+                if (HasUserName(username))
+                {
+                    MessageBox.Show("Van már ilyen felhasználónév.");
+                }
+                else if(IsValid)
+                {
+
+                    if (password == RetryPassowrd)
+                    {
+                        UserModel NewUser = new UserModel(FirstName, LastName, username, password);
+                        context.Users.Add(UserMapper.ModelToEntity(NewUser));
+                        context.SaveChanges();
+                        MessageBox.Show("Sikeres regisztáció!");
+                    }
+                    else
+                    {
+                        MessageBox.Show("The two passwords doesn't match!");
+                    }
+                }
+            }
+        }
+
+
+
+
+        public string Error
+        {
+            get;
+        }
+
+        Dictionary<string, string> _errors = new Dictionary<string, string>();
+
+
+        private bool _isValid = false;
+
+        public bool IsValid
+        {
+            get { return _isValid; }
+            set { _isValid = value; }
+        }
+        private Dictionary<string, List<string>> _validationErrors = new Dictionary<string, List<string>>();
+
+        public event EventHandler<DataErrorsChangedEventArgs>
+           ErrorsChanged = delegate { };
+        public IEnumerable GetErrors(string propertyName)
+        {
+
+            if (_validationErrors.ContainsKey(propertyName))
+                return _validationErrors[propertyName];
+            else
+                return null;
+        }
+
+        public bool HasErrors
+        {
+            get { return _validationErrors.Count > 0; }
+        }
+
+        private string ValidateProperty(string propertyName, object value)
+        {
+            string error = string.Empty;
+            var results = new List<ValidationResult>();
+            var result = Validator.TryValidateProperty(value, new ValidationContext(this, null, null)
+            {
+                MemberName = propertyName
+            }, results);
+
+            if (!result)
+            {
+                if (_validationErrors.ContainsKey(propertyName))
+                    _validationErrors[propertyName].Clear();
+
+                var messages = results.Select(r => r.ErrorMessage).ToList();
+                _validationErrors.Add(propertyName, messages);
+
+            }
+
+            return error;
+        }
+
+        public void OnErrorsChanged(string propertyName)
+        {
+            if (ErrorsChanged != null)
+                ErrorsChanged(this, new DataErrorsChangedEventArgs(propertyName));
+        }
+
+        public void Validate()
+        {
+            var validationContext = new ValidationContext(this, null, null);
+            var results = new List<ValidationResult>();
+            var result = Validator.TryValidateObject(this, validationContext, results, true);
+
+           
+            var propNames = _validationErrors.Keys.ToList();
+            _validationErrors.Clear();
+            propNames.ForEach(pn => OnErrorsChanged(pn));
+
+            if (!result)
+            {
+                var q = from r in results
+                        from m in r.MemberNames
+                        group r by m into g
+                        select g;
+
+                foreach (var prop in q)
+                {
+                    var messages = prop.Select(r => r.ErrorMessage).ToList();
+
+                    if (_errors.ContainsKey(prop.Key))
+                    {
+                        _validationErrors.Remove(prop.Key);
+                    }
+                    _validationErrors.Add(prop.Key, messages);
+                    OnErrorsChanged(prop.Key);
+                }
+
+                IsValid = false;
+            }
+            else
+            {
+                IsValid = true;
             }
         }
     }
