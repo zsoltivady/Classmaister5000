afa341d4206ba49f2296c1d7d39c5b0b2a26141d
Unknown <barsimartin@gmail.com>
2018-11-24 21:18:21 +0100

    Kurzus hozzáad navigálás

--- Orarend_osszerako/Orarend_osszerako/View/Orarend.xaml.cs
+++ Orarend_osszerako/Orarend_osszerako/View/Orarend.xaml.cs
@@ -24,5 +24,10 @@ namespace Orarend_osszerako.View
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            new KurzusHozzaad().Show();
+        }
     }
 }
