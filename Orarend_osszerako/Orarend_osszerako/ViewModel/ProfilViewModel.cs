acd98f029996fe922b6ddccb149f40a952e5917d
Unknown <barsimartin@gmail.com>
2018-12-05 18:13:39 +0100

    Profil bájnding

---
+++ Orarend_osszerako/Orarend_osszerako/ViewModel/ProfilViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Orarend_osszerako.Command;
+using System.Threading.Tasks;
+
+namespace Orarend_osszerako.ViewModel
+{
+    class ProfilViewModel:ObservableObject
+    {
+
+    }
+}
