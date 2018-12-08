37ec5032280a61561e0c4d804f4132def22e49eb
dezoox <43348651+dezoox@users.noreply.github.com>
2018-12-08 17:13:04 +0100

    xaml.cs fájlokból felesleges namespacek törlése

--- Orarend_osszerako/Orarend_osszerako/View/LoginPage.xaml.cs
+++ Orarend_osszerako/Orarend_osszerako/View/LoginPage.xaml.cs
@@ -1,18 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using Orarend_osszerako;
 
 namespace Orarend_osszerako.View
 {
@@ -30,18 +17,5 @@ namespace Orarend_osszerako.View
         {
             userName.Focus();
         }
-
-        //private void Login_Click(object sender, RoutedEventArgs e)
-        //{
-
-        //}
-
-        //                         Majd a LoginViewModel.cs-ben!!
-        //private void Login_Click(object sender, RoutedEventArgs e)
-        //{
-        //    new TimeTableWindow().Show();
-        //    Window.GetWindow(this).Close();
-        //    // TODO Itt kell csekkolni a login adatokat és validálni!!!!!
-        //}
     }
 }
