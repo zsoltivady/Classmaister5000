d78253dd20efaa3dcc0e76ae98f26584e8a9ba00
ldaniel94 <43348717+ldaniel94@users.noreply.github.com>
2018-12-18 13:48:22 +0100

    UnitTest (László Dániel)
    A RequiredValidation IsValid metódusához megírtam a UnitTest-et.

---
+++ Orarend_osszerako/UnitTests/RequiredValidationUnitTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Orarend_osszerako.Validation;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class RequiredValidationUnitTests
+    {
+        [TestMethod]
+        public void IsValid_NotNullOrEmpty_True()
+        {
+            string szoveg = "László Dániel UnitTest-je";
+
+            bool result = Required.IsValid(szoveg);
+
+            Assert.IsTrue(result);            
+        }
+    }
+}
